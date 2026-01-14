b50732c9e230860b136ff8b4b2a8921f08149967
rama8760 <ramaennab123@gmail.com>
2026-01-14 14:04:11 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,271 +1,82 @@
-﻿//using System;
+﻿using System;
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        int[,] arr = new int[3, 3];
-
-
-//        for (int i = 0; i < 3; i++)
-//        {
-//            for (int j = 0; j < 3; j++)
-//            {
-//                Console.Write($"Enter value for [{i},{j}]: ");
-//                arr[i, j] = int.Parse(Console.ReadLine());
-//            }
-//        }
-
-
-//        Console.WriteLine("\nArray:");
-//        for (int i = 0; i < 3; i++)
-//        {
-//            for (int j = 0; j < 3; j++)
-//            {
-//                Console.Write(arr[i, j] + "\t");
-//            }
-//            Console.WriteLine();
-//        }
-//    }
-//}
-//part2
-//using System;
-
-//class Program
-//{
-//    static void Main()
-//    {
-//        int[,] arr =
-//        {
-//            {1, 2, 3, 4},
-//            {5, 6, 7, 8},
-//            {9, 10, 11, 12},
-//            {13, 14, 15, 16}
-//        };
-
-
-//        for (int i = 0; i < 4; i++)
-//        {
-//            int rowSum = 0;
-//            for (int j = 0; j < 4; j++)
-//                rowSum += arr[i, j];
-
-//            Console.WriteLine($"Sum of row {i}: {rowSum}");
-//        }
-
-
-//        for (int j = 0; j < 4; j++)
-//        {
-//            int colSum = 0;
-//            for (int i = 0; i < 4; i++)
-//                colSum += arr[i, j];
-
-//            Console.WriteLine($"Sum of column {j}: {colSum}");
-//        }
-//    }
-//}
-////part3
-//using System;
-
-//class Program
-//{
-//    static void Main()
-//    {
-//        int[,] arr = new int[5, 5];
-//        Random rand = new Random();
-
-
-//        for (int i = 0; i < 5; i++)
-//            for (int j = 0; j < 5; j++)
-//                arr[i, j] = rand.Next(1, 51);
-
-
-//        Console.WriteLine("Array:");
-//        for (int i = 0; i < 5; i++)
-//        {
-//            for (int j = 0; j < 5; j++)
-//                Console.Write(arr[i, j] + "\t");
-//            Console.WriteLine();
-//        }
-
-
-//        for (int i = 0; i < 5; i++)
-//        {
-//            int max = arr[i, 0];
-//            for (int j = 1; j < 5; j++)
-//                if (arr[i, j] > max)
-//                    max = arr[i, j];
-
-//            Console.WriteLine($"Max in row {i}: {max}");
-//        }
-
-
-//        for (int j = 0; j < 5; j++)
-//        {
-//            int min = arr[0, j];
-//            for (int i = 1; i < 5; i++)
-//                if (arr[i, j] < min)
-//                    min = arr[i, j];
-
-//            Console.WriteLine($"Min in column {j}: {min}");
-//        }
-//    }
-//}
-//part4
-using System;
-
-class Program
+class Car
 {
-    static void Main()
+   
+    private string brand;
+    private string model;
+    private int year;
+
+    public string Brand
     {
-        int[][] jagged = new int[3][];
-        jagged[0] = new int[2];
-        jagged[1] = new int[3];
-        jagged[2] = new int[4];
-
-        for (int i = 0; i < jagged.Length; i++)
-        {
-            for (int j = 0; j < jagged[i].Length; j++)
-            {
-                Console.Write($"Enter value for row {i}, index {j}: ");
-                jagged[i][j] = int.Parse(Console.ReadLine());
-            }
-        }
-
-        Console.WriteLine("\nJagged Array:");
-        for (int i = 0; i < jagged.Length; i++)
-        {
-            for (int j = 0; j < jagged[i].Length; j++)
-                Console.Write(jagged[i][j] + " ");
-            Console.WriteLine();
-        }
+        get { return brand; }
+        set { brand = value; }
     }
-}
-//part5
-using System;
 
-class Program
-{
-    static void Main()
+    public string Model
     {
-        Console.Write("Enter number of rows: ");
-        int rows = int.Parse(Console.ReadLine());
-
-        int[][] jagged = new int[rows][];
-
-        for (int i = 0; i < rows; i++)
-        {
-            Console.Write($"Enter length of row {i}: ");
-            int length = int.Parse(Console.ReadLine());
-            jagged[i] = new int[length];
-
-            for (int j = 0; j < length; j++)
-            {
-                Console.Write($"Enter value [{i}][{j}]: ");
-                jagged[i][j] = int.Parse(Console.ReadLine());
-            }
-        }
-
-        Console.WriteLine("\nJagged Array:");
-        for (int i = 0; i < jagged.Length; i++)
-        {
-            foreach (int val in jagged[i])
-                Console.Write(val + " ");
-            Console.WriteLine();
-        }
+        get { return model; }
+        set { model = value; }
     }
-}
-//part6
-using System;
 
-class Program
-{
-    static void Main()
+    public int Year
     {
-        int[][] jagged = new int[5][];
-        Random rand = new Random();
-
-        for (int i = 0; i < 5; i++)
-        {
-            jagged[i] = new int[i + 1];
-            int sum = 0;
-
-            for (int j = 0; j < jagged[i].Length; j++)
-            {
-                jagged[i][j] = rand.Next(1, 101);
-                sum += jagged[i][j];
-            }
-
-            Console.WriteLine($"Sum of row {i + 1}: {sum}");
-        }
+        get { return year; }
+        set { year = value; }
     }
-}
-//part7
-using System;
-using System.Collections.Generic;
 
-class Program
-{
-    static void Main()
+
+    public void Start()
     {
-        List<int> numbers = new List<int>();
-
-        for (int i = 0; i < 5; i++)
-        {
-            Console.Write("Enter number: ");
-            numbers.Add(int.Parse(Console.ReadLine()));
-        }
-
-        Console.WriteLine("Numbers:");
-        foreach (int n in numbers)
-            Console.WriteLine(n);
+        Console.WriteLine("The car is starting");
     }
-}
-//part8
-using System;
-using System.Collections.Generic;
 
-class Program
-{
-    static void Main()
+    public void Drive()
     {
-        List<int> numbers = new List<int>();
-        int input;
+        Console.WriteLine("The car is driving");
+    }
 
-        while (true)
-        {
-            Console.Write("Enter number (-1 to stop): ");
-            input = int.Parse(Console.ReadLine());
-
-            if (input == -1)
-                break;
-
-            numbers.Add(input);
-        }
-
-        Console.WriteLine("\nElements:");
-        foreach (int n in numbers)
-            Console.Write(n + " ");
-
-        int sum = 0, max = numbers[0], min = numbers[0];
-
-        foreach (int n in numbers)
-        {
-            sum += n;
-            if (n > max) max = n;
-            if (n < min) min = n;
-        }
-
-        Console.WriteLine($"\nAverage: {(double)sum / numbers.Count}");
-        Console.WriteLine($"Max: {max}");
-        Console.WriteLine($"Min: {min}");
-
-        numbers.Sort();
-        Console.WriteLine("Sorted:");
-        foreach (int n in numbers)
-            Console.Write(n + " ");
+    public void Stop()
+    {
+        Console.WriteLine("The car has stopped");
     }
 }
 
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Car car1 = new Car();
+        car1.Brand = "Toyota";
+        car1.Model = "Corolla";
+        car1.Year = 2020;
 
+        Car car2 = new Car();
+        car2.Brand = "BMW";
+        car2.Model = "X5";
+        car2.Year = 2023;
+
+     
+        car1.Start();
+        car1.Drive();
+        car1.Stop();
+
+        Console.WriteLine();
+
+        car2.Start();
+        car2.Drive();
+        car2.Stop();
+
+        Console.WriteLine("\nCar 1:");
+        Console.WriteLine("Brand: " + car1.Brand);
+        Console.WriteLine("Model: " + car1.Model);
+        Console.WriteLine("Year: " + car1.Year);
+
+        Console.WriteLine("\nCar 2 :");
+        Console.WriteLine("Brand: " + car2.Brand);
+        Console.WriteLine("Model: " + car2.Model);
+        Console.WriteLine("Year: " + car2.Year);
+    }
+}
 
